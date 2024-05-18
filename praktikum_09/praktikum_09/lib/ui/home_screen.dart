22b84e8dbc3c5f1a8786a30722166df6ae6049cf
Farhan yoga dwi Laksana <farhanyogadwilaksana@gmail.com>
2024-05-18 13:53:29 +0700

    praktikum_09

--- praktikum_09/praktikum_09/lib/ui/home_screen.dart
+++ praktikum_09/praktikum_09/lib/ui/home_screen.dart
@@ -1,18 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:praktikum_09/ui/login.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-          child: Text(
-        "HOME SCREEN",
-        style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-            color: Color(0xff3D4DE0)),
-      )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "HOME SCREEN",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Color(0xff3D4DE0),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () async {
+                FirebaseAuth.instance
+                    .signOut()
+                    .then((value) => Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginScreen(),
+                        ),
+                        (route) => false));
+              },
+              child: const Text('Keluar'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- praktikum_09/praktikum_09/lib/ui/login.dart
+++ praktikum_09/praktikum_09/lib/ui/login.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:praktikum_09/bloc/login/login_cubit.dart';
+import 'package:praktikum_09/ui/home_screen.dart';
+import 'package:praktikum_09/ui/phone_auth_screen.dart';
 
 import '../utils/routes.dart';
 
@@ -15,6 +19,23 @@ class _LoginScreenState extends State<LoginScreen> {
   final emailEdc = TextEditingController();
   final passEdc = TextEditingController();
   bool passInvisible = false;
+
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: gAuth.accessToken,
+      idToken: gAuth.idToken,
+    );
+    return await FirebaseAuth.instance.signInWithCredential(credential).then(
+          (value) async => await Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => const HomeScreen()),
+            (route) => false,
+          ),
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,7 +44,7 @@ class _LoginScreenState extends State<LoginScreen> {
           if (state is LoginLoading) {
             ScaffoldMessenger.of(context)
               ..hideCurrentSnackBar()
-              ..showSnackBar(SnackBar(content: Text('Loading..')));
+              ..showSnackBar(const SnackBar(content: Text('Loading..')));
           }
           if (state is LoginFailure) {
             ScaffoldMessenger.of(context)
@@ -34,7 +55,6 @@ class _LoginScreenState extends State<LoginScreen> {
               ));
           }
           if (state is LoginSuccess) {
-            // context.read<AuthCubit>().loggedIn();
             ScaffoldMessenger.of(context)
               ..hideCurrentSnackBar()
               ..showSnackBar(SnackBar(
@@ -45,39 +65,32 @@ class _LoginScreenState extends State<LoginScreen> {
           }
         },
         child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 70),
+          margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 70),
           child: ListView(
             children: [
-              Text(
+              const Text(
                 "Login",
                 style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold,
-                    color: Color(0xff3D4DE0)),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                "Silahkan masukan e-mail dan password anda",
-                style: TextStyle(
-                  fontSize: 16,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xff3D4DE0),
                 ),
               ),
-              SizedBox(
-                height: 25,
+              const SizedBox(height: 15),
+              const Text(
+                "Silahkan masukan e-mail dan password anda",
+                style: TextStyle(fontSize: 16),
               ),
-              Text(
+              const SizedBox(height: 25),
+              const Text(
                 "e-mail",
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
               TextFormField(
                 controller: emailEdc,
               ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
+              const SizedBox(height: 10),
+              const Text(
                 "password",
                 style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
@@ -97,46 +110,78 @@ class _LoginScreenState extends State<LoginScreen> {
                 ),
                 obscureText: !passInvisible,
               ),
-              SizedBox(
-                height: 50,
-              ),
+              const SizedBox(height: 50),
               ElevatedButton(
-                  onPressed: () {
-                    context
-                        .read<LoginCubit>()
-                        .login(email: emailEdc.text, password: passEdc.text);
-                  },
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0xff3D4DE0),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10))),
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24,
-                        color: Colors.white),
-                  )),
-              SizedBox(
-                height: 25,
+                onPressed: () {
+                  context
+                      .read<LoginCubit>()
+                      .login(email: emailEdc.text, password: passEdc.text);
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xff3D4DE0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text(
+                  "Login",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    color: Colors.white,
+                  ),
+                ),
               ),
+              const SizedBox(height: 30),
               Row(
-                mainAxisAlignment:
-                    MainAxisAlignment.center, // Menengahkan elemen horizontal
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("Belum punya akun ?"),
-                  TextButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/register');
-                      },
-                      child: Text(
-                        "Daftar",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Color(0xff3D4DE0)),
-                      ))
+                  GestureDetector(
+                    onTap: signInWithGoogle,
+                    child: const CircleAvatar(
+                      radius: 20.0,
+                      backgroundImage: NetworkImage(
+                        'https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png',
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 30),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => PhoneAuthScreen()),
+                      );
+                    },
+                    child: const CircleAvatar(
+                      radius: 20.0,
+                      backgroundImage: NetworkImage(
+                        'https://img2.pngdownload.id/20180331/yaq/kisspng-logo-telephone-company-yell-calling-5abf775edb9bb5.0259369015224973748995.jpg',
+                      ),
+                    ),
+                  ),
                 ],
-              )
+              ),
+              const SizedBox(height: 25),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Belum punya akun ?"),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/register');
+                    },
+                    child: const Text(
+                      "Daftar",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xff3D4DE0),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
